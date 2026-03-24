0905d4cf0ad794f756965d4975d940c5f04ef1e8
Shounak96 <shounakpowar96@gmail.com>
2026-03-24 12:19:47 -0500

    Updated some files

--- components/hero.jsx
+++ components/hero.jsx
@@ -47,11 +47,7 @@ const HeroSection = () => {
                 Get Started
               </Button>
             </Link>
-            <Link href="https://www.youtube.com/roadsidecoder">
-              <Button size="lg" variant='outline' className="px-8">
-                Watch Demo
-              </Button>
-            </Link>
+            
           </div>
           <div className="hero-image-wrapper">
             <div ref={imageRef} className="hero-image">
